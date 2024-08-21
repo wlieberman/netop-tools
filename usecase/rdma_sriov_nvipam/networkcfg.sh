60bca79d4da65828a52cb10b6b58ade1ef1188bb
Thomas Huff <55858677+thuffmlx@users.noreply.github.com>
2024-08-21 07:51:39 -0700

    Add files via upload

--- usecase/rdma_sriov_nvipam/networkcfg.sh
+++ usecase/rdma_sriov_nvipam/networkcfg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# setup the host networks, and make the nvipam ip pool
+# setup the host networks, and make the whereabouts ip pool
 #
 source ./netop.cfg
-./ops/mk-sriovnet-nvipam-cr.sh ${NETOP_NETWORK_NAME} a b c
+./ops/mk-hostnet-ipam-cr.sh ${NETOP_NETWORK_NAME} a b c
 NETWORKS=$(ls ${NETOP_NETWORK_NAME}*.yaml)
 for NETWORK in ${NETWORKS[@]};do
   kubectl apply -f ./${NETWORK}
@@ -11,9 +11,9 @@ done
 #
 # verify the network devices
 #
-kubectl get  ${NETOP_NETWORK_TYPE}
+kubectl get ${NETOP_NETWORK_TYPE}
 #
 # make sure the ip pool is created
 #
-./ops/mk-nvipam.sh
-kubectl apply -f ippool.yaml
+./ops/mk-whereabouts.sh
+kubectl apply -f whereabouts.yaml
