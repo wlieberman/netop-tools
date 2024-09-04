91d36c84f178c01dbdf9f4ef53c681d5286455b8
Aleksey Senin <alekseys@nvidia.com>
2024-09-04 14:27:30 -0400

    Update sandbox_images to 3.10

--- install/fixes/fixcontainerd.sh
+++ install/fixes/fixcontainerd.sh
@@ -2,7 +2,10 @@
 #
 # https://github.com/containerd/containerd/issues/6009
 #
-containerd config default | sudo tee /etc/containerd/config.toml
-sed -i 's/SystemdCgroup =.*/SystemdCgroup = true/' /etc/containerd/config.toml
+CONTAINERD_CONFIG=/etc/containerd/config.toml
+
+containerd config default | sudo tee $CONTAINERD_CONFIG
+sed -i 's#SystemdCgroup =.*#SystemdCgroup = true#' $CONTAINERD_CONFIG
+sed -i 's#sandbox_image = "registry.k8s.io/pause:3.8"#sandbox_image = "registry.k8s.io/pause:3.10"#' $CONTAINERD_CONFIG
 systemctl restart containerd
 systemctl restart kubelet
