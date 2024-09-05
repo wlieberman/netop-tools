14bf59789ec82aa9a629c94ad9e5e86c92e99819
Aleksey Senin <alekseys@nvidia.com>
2024-09-04 20:44:12 -0400

    Fixed typo in include netop.cfg

--- usecase/hostnet_rdma_sriov_ipam/networkcfg.sh
+++ usecase/hostnet_rdma_sriov_ipam/networkcfg.sh
@@ -2,7 +2,7 @@
 #
 # setup the host networks, and make the whereabouts ip pool
 #
-source ${NETOOP_ROOT_DIR}/netop.cfg
+source ${NETOP_ROOT_DIR}/netop.cfg
 #./ops/mk-hostnet-ipam-cr.sh ${NETOP_NETWORK_NAME} a b c
 ./ops/mk-hostnet-ipam-cr.sh ${NETOP_NETWORK_NAME} a b
 NETWORKS=$(ls ${NETOP_NETWORK_NAME}*.yaml)
