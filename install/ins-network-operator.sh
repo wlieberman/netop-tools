065302874427ee66cc60c221710b307904284d51
thuff <thuff@nvidia.com>
2024-09-06 06:30:21 +0300

    generate the values.yaml file as part of the network-operator install step

--- install/ins-network-operator.sh
+++ install/ins-network-operator.sh
@@ -3,6 +3,7 @@
 # install the network operator.
 #
 source ${NETOP_ROOT_DIR}/global_ops.cfg
+USECASE_DIR="${NETOP_ROOT_DIR}/usecase/${USECASE}"
 systemctl restart kubelet
 #helm install -n ${NETOP_NAMESPACE} --create-namespace network-operator ./network-operator
 X=`kubectl get ns | grep -c "^${NETOP_NAMESPACE} "`
@@ -10,5 +11,6 @@ if [ "${X}" = "0" ];then
   kubectl create ns ${NETOP_NAMESPACE}
 fi
 ${NETOP_ROOT_DIR}/install/mksecret.sh
-helm install -n ${NETOP_NAMESPACE} network-operator nvidia/network-operator -f ${NETOP_ROOT_DIR}/usecase/${USECASE}/values.yaml
+${USECASE_DIR}/mk-values.sh
+helm install -n ${NETOP_NAMESPACE} network-operator nvidia/network-operator -f ${USECASE_DIR}/values.yaml
 ${NETOP_ROOT_DIR}/install/applycrds.sh 
