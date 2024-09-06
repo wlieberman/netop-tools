80caff637d485835ecaa108f5ef0e934a1280d2d
thuff <thuff@nvidia.com>
2024-09-06 05:47:27 +0300

    fix hostnet_rdma_sriov_nvipam usecase files

--- usecase/hostnet_rdma_sriov_nvipam/networkcfg.sh
+++ usecase/hostnet_rdma_sriov_nvipam/networkcfg.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
-# setup the host networks, and make the nvipam ip pool
+# setup the host networks, and make the whereabouts ip pool
 #
 source ${NETOP_ROOT_DIR}/global_ops.cfg
-${NETOP_ROOT_DIR}/ops/mk-hostnet-nvipam-cr.sh ${NETOP_NETWORK_NAME} a b
+
+for DEVDEF in ${NETOP_NETLIST[@]};do
+  NIDX=`echo ${DEVDEF}|cut -d',' -f1`
+  NIDXLST="${NIDXLST} ${NIDX}"
+done
+${NETOP_ROOT_DIR}/ops/mk-hostnet-nvipam-cr.sh ${NETOP_NETWORK_NAME} ${NIDXLST}
 NETWORKS=$(ls ${NETOP_NETWORK_NAME}*.yaml)
 for NETWORK in ${NETWORKS[@]};do
   kubectl apply -f ./${NETWORK}
@@ -11,9 +16,10 @@ done
 #
 # verify the network devices
 #
-kubectl get  ${NETOP_NETWORK_TYPE}
+kubectl get ${NETOP_NETWORK_TYPE}
 #
 # make sure the ip pool is created
 #
 ${NETOP_ROOT_DIR}/ops/mk-nvipam.sh
 kubectl apply -f ippool.yaml
+
