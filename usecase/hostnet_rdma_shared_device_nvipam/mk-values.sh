0690baed00c33ac6fc0c62a7150331e46833737a
thuff <thuff@nvidia.com>
2024-09-06 05:52:19 +0300

    fix hostnet_rdma_shared_device_nvipam config files

--- usecase/hostnet_rdma_shared_device_nvipam/mk-values.sh
+++ usecase/hostnet_rdma_shared_device_nvipam/mk-values.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 #
-source ${NETOP_ROOT_DIR}/global_ops.cfg
+source "${NETOP_ROOT_DIR}/global_ops.cfg"
+
 cat <<HEREDOC1>./values.yaml
 nfd:
   enabled: true
 sriovNetworkOperator:
-  enabled: true
+  enabled: false
 HEREDOC1
 if [ "${PROD_VER}" = "0" ];then
 cat <<HEREDOC2>>./values.yaml
@@ -17,6 +18,7 @@ imagePullSecrets: [ngc-image-secret]   # <- specify your created pull secrets fo
 HEREDOC2
 fi
 cat <<HEREDOC3>>./values.yaml
+# NicClusterPolicy CR values:
 deployCR: true
 nvIpam:
   deploy: true
@@ -31,7 +33,33 @@ ofedDriver:
   - name: CREATE_IFNAMES_UDEV
     value: "true"
 rdmaSharedDevicePlugin:
-  deploy: false
+  deploy: true
+  resources:
+HEREDOC3
+for DEVDEF in ${NETOP_NETLIST[@]};do
+  NIDX=`echo ${DEVDEF}|cut -d',' -f1`
+  DEVICEID=`echo ${DEVDEF}|cut -d',' -f2`
+  NETOP_HCAMAX=`echo ${DEVDEF}|cut -d',' -f3`
+  DEVNAMES=`echo ${DEVDEF}|cut -d',' -f4-12`
+echo "    - name: ${NETOP_RESOURCE}_${NIDX}" >>./values.yaml
+###  if [ "${NETOP_VENDOR}" != "" ];then
+###echo "      vendors: [${NETOP_VENDOR}]" >>./values.yaml
+###  fi
+###  if [ "${DEVICEID}" != "" ];then
+###echo "      deviceIDs: [${DEVICEID}]" >>./values.yaml
+###  fi
+###  if [ "${NETOP_HCAMAX}" != "" ];then
+###echo "      rdmaHcaMax: ${NETOP_HCAMAX}" >>./values.yaml
+###  fi
+  if [ "${DEVNAMES}" != "" ];then
+    if [[ $string == *:* ]]; then
+      echo "PCIe:BFD device id not supported by rdmaSharedDevicePlugin"
+    else
+echo "      ifNames: [${DEVNAMES}]" >>./values.yaml
+    fi
+  fi
+done
+cat <<HEREDOC4>>./values.yaml
 
 sriovDevicePlugin:
   deploy: false
@@ -44,4 +72,4 @@ secondaryNetwork:
     deploy: true
   ipamPlugin:
     deploy: false
-HEREDOC3
+HEREDOC4
