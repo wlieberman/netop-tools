a687199d98d210af1c5927a3f63ec29419849294
thuff <thuff@nvidia.com>
2024-09-09 02:56:21 +0300

    more config reorg fixes

--- ops/mk-whereabouts.sh
+++ ops/mk-whereabouts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 #
-# use whereabouts install of ipam for small clusters
+# use ${IPAM_TYPE} install of ipam for small clusters
 #
 source ${NETOP_ROOT_DIR}/global_ops.cfg
-cat << HEREDOC > ./whereabouts.yaml
+cat << HEREDOC > ./${IPAM_TYPE}.yaml
 apiVersion: mellanox.com/v1alpha1
 kind: ${NETOP_NETWORK_TYPE}
 metadata:
@@ -16,12 +16,12 @@ spec:
       "type": "${IPAM_TYPE}",
       "datastore": "kubernetes",
       "kubernetes": {
-        "kubeconfig": "/etc/cni/net.d/whereabouts.d/whereabouts.kubeconfig"
+        "kubeconfig": "/etc/cni/net.d/${IPAM_TYPE}.d/${IPAM_TYPE}.kubeconfig"
       },
       "range": "${NETOP_NETWORK_RANGE}",
       "exclude": [],
-      "log_file" : "/var/log/whereabouts.log",
+      "log_file" : "/var/log/${IPAM_TYPE}.log",
       "log_level" : "info"
     }
 HEREDOC
-cat ./whereabouts.yaml
+cat ./${IPAM_TYPE}.yaml
