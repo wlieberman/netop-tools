0a4af7397140bcc475373d9b66337247ab8d8153
thuff <thuff@nvidia.com>
2024-09-10 09:07:29 +0300

    file name fixes

--- usecase/sriovibnet_rdma/networkcfg.sh
+++ usecase/sriovibnet_rdma/networkcfg.sh
@@ -16,7 +16,7 @@ for DEVDEF in ${NETOP_NETLIST[@]};do
   NDEV=`echo ${DEVDEF}|cut -d',' -f4-20`
   ${NETOP_ROOT_DIR}/ops/mk-sriovibpolicy.sh ${NIDX} ${NDEV}
   kubectl apply -f sriovibnetwork-node-policy-${NIDX}.yaml
-  ${NETOP_ROOT_DIR}/ops/mk-network-attachment.sh ${NIDX}
+  ${NETOP_ROOT_DIR}/ops/mk-sriovib-network-attachment.sh ${NIDX}
   kubectl apply -f "./Network-Attachment-Definitions-${NIDX}.yaml"
   if [ "${IPAM_TYPE}" = "nv-ipam" ];then
     ${NETOP_ROOT_DIR}/ops/mk-sriovibnet-nvipam-cr.sh ${NIDX}
